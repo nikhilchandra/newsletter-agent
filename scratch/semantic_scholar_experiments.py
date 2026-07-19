6707c6d28472b3a81d31f3573825a5acdd7c34ec
Nikhil Chandra <8987496+nikhilchandra@users.noreply.github.com>
2026-07-18 19:32:33 -0500

    chore: clean up semantic_scholar_experiments.py

--- scratch/semantic_scholar_experiments.py
+++ scratch/semantic_scholar_experiments.py
@@ -58,10 +58,8 @@ REQUEST_RATE = 0 # requests per second - assumes API key
 
 load_dotenv()
 SEMANTIC_SCHOLAR_API_KEY = os.environ.get("SEMANTIC_SCHOLAR_API_KEY")
-print(SEMANTIC_SCHOLAR_API_KEY)
 
 def test_get_paper_title():
-    print('test_get_paper_title()')
     # given a paper ID, get its title using this basic request
     #https://api.semanticscholar.org/graph/v1/paper/{paper_id}
     endpoint='https://api.semanticscholar.org/graph/v1/paper'
@@ -75,12 +73,9 @@ def test_get_paper_title():
         print(r.headers.get("Retry-After"))
     else:
         print(r.json()) # use .json() function to get result as dict
-        # {'paperId': '649def34f8be52c8b66281af98ae884c09aef38b', 'title': 'Construction of the 
-        # Literature Graph in Semantic Scholar'}
     time.sleep(REQUEST_RATE)
 
 def test_get_paper_details():
-    print('test_get_paper_details')
     # There is only one supported parameter: fields. This parameter takes 
     # a comma-separated string of field names. This tells the API what 
     # information to return in the response. Here are the supported fields:
@@ -97,34 +92,49 @@ def test_get_paper_details():
         print(f'Unexpected status code {r.status_code}')
     else:
         print(r.json())
-        # {'paperId': '649def34f8be52c8b66281af98ae884c09aef38b', 
-        # 'externalIds': {'MAG': '2801930304', 'DBLP': 'conf/naacl/AmmarGBBCDDEFHK18', 
-        # 'ACL': 'N18-3011', 'ArXiv': '1805.02262', 
-        # 'DOI': '10.18653/v1/N18-3011', 'CorpusId': 19170988}, 
-        # 'corpusId': 19170988, 
-        # 'url': 'https://www.semanticscholar.org/paper/649def34f8be52c8b66281af98ae884c09aef38b', 
-        # 'title': 'Construction of the Literature Graph in Semantic Scholar'}
     time.sleep(REQUEST_RATE)
 
-# def test_get_details_for_multiple_papers():
-#     print('test_get_details_for_multiple_papers()')
-#     # use requests.post with headers dict containing list of paper ids keyed on 'ids'
-#     url='https://api.semanticscholar.org/graph/v1/paper/batch'
-#     params={'fields':'paperId,title,referenceCount,citationCount'}
-#     headers={'x-api-key':SEMANIC_SCHOLAR_API_KEY}
-#     headers|={'ids': ['649def34f8be52c8b66281af98ae884c09aef38b', 'ARXIV:1805.02262']}
-#     r = requests.post(url, params=params, headers=headers)
-#     if r.status_code == 429:
-#         print('Rate limit exceeded.')
-#     elif r.status_code == 503:
-#         print('Server is down.')
-#     elif r.status_code != 200:
-#         print(r'Unexpected status code {r.status_code}')
-#     else:
-#         print(r.json())
+def test_get_details_for_multiple_papers():
+    # use requests.post with headers dict containing list of paper ids keyed on 'ids'
+    url='https://api.semanticscholar.org/graph/v1/paper/batch'
+    params={'fields':'paperId,title,referenceCount,citationCount'}
+    json={'ids': ['649def34f8be52c8b66281af98ae884c09aef38b', 'ARXIV:1805.02262']}
+    headers={'x-api-key':SEMANTIC_SCHOLAR_API_KEY}
+    r = requests.post(url, params=params, headers=headers, json=json)
+    if r.status_code == 429:
+        print('Rate limit exceeded.')
+    elif r.status_code == 503:
+        print('Server is down.')
+    elif r.status_code != 200:
+        print(r'Unexpected status code {r.status_code}')
+    else:
+        print(r.json())
     
-#     time.sleep(REQUEST_RATE)
+    time.sleep(REQUEST_RATE)
+
+def test_search_for_papers_in_bulk():
+    
+    # accepted fields:
+    #   query - + for AND, | for OR, - for negation, "" for collecting terms into phrase, * for matching prefix
+    #       ( and ) for precedence, ~N after a word matches within the edit distance of N, 
+
+    url='https://api.semanticscholar.org/graph/v1/paper/search/bulk'
+    params={'fields':'paperId,title'}
+    params|={'query':'prosthetic limbs'}
+    params|={'sort':'publicationDate:desc'} # paperId, publicationDate, citationCount + ':' + 'asc/desc'
+    headers={'x-api-key':SEMANTIC_SCHOLAR_API_KEY}
+    r = requests.get(url, params=params, headers=headers)
+    if r.status_code != 200:
+        print(f'Status code: {r.status_code}')
+        if r.status_code == 429:
+            print(r.text[:300])
+    else:
+        print(r.json())
+    
+    time.sleep(REQUEST_RATE)
 
 if __name__ == "__main__":
-    test_get_paper_title()
+    # test_get_paper_title()
     # test_get_paper_details()
+    # test_get_details_for_multiple_papers()
+    test_search_for_papers_in_bulk()
